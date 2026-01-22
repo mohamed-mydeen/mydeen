5ccbf5efd052cc5e2b615148c0e4dbb6cd69b265
Mohamed Mydeen Shahabudeen M <156303564+mohamed-mydeen@users.noreply.github.com>
2026-01-23 00:36:35 +0530

    Update educationData.ts

--- src/data/educationData.ts
+++ src/data/educationData.ts
@@ -40,7 +40,7 @@ export const educationData: EducationType[] = [
     ]
   },
   {
-    period: "2 Month",
+    period: " 2 Month",
     title: "Fullstack Developement Intern",
     organization: "Asta Solution",
     description: "Working on developing and maintaining web applications using Java Springboot,Reactjs,nodejs and CSS, and MySQL. Collaborating with cross-functional teams to deliver high-quality products.",
